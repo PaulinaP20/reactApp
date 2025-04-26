e15eb344f59f2bb90a63ee4144179ec353b099f3
Paulina Partyka <paulinapartyka2@gmail.com>
2025-04-26 15:04:06 +0200

    add deletePosts

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Button, Card, Container, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle } from 'react-bootstrap';
+import { removePost } from '../../postsRedux.js';
+import {useState} from 'react';
 
 const Post = () => {
     const { id } = useParams();
+
+    const post=useSelector(state=> state.posts.find(post=>post.id===id));
+
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleDelete = () => {
+      dispatch(removePost(id));
+      setShowModal(false);
+      navigate('/');
+    }
+
     return (
-      <div>
-        <h1>Post Page - Post ID: {id}</h1>
-      </div>
+      <Container className="mt-4">
+      <Card className="shadow-lg w-50 mx-auto">
+        <Card.Body>
+          <div className="d-flex justify-content-between align-items-start">
+            <div>
+            <h2>{post.title}</h2>
+                    <p><strong>Author:</strong> {post.author}</p>
+                    <p><strong>Published:</strong> {post.publishedDate}</p>
+                    <p>{post.shortDescription}</p>
+            </div>
+            <div>
+              <Button
+                variant="primary"
+                onClick={() => navigate(`/post/edit/${post.id}`)}
+                className="ms-2"
+              >
+                Edit
+              </Button>
+              <Button
+                variant="danger"
+                className="ms-2"
+                onClick={()=>setShowModal(true)}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+          <Card.Text className="mt-3">
+            {post.content}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Modal show={showModal} onHide={()=>setShowModal(false)}>
+
+        <ModalHeader closeButton>
+          <ModalTitle>Are you sure?</ModalTitle>
+        </ModalHeader>
+
+        <ModalBody>Are you sure you want to delete this post?</ModalBody>
+
+        <ModalFooter>
+          <Button variant="secondary" onClick={()=>setShowModal(false)}>Cancel</Button>
+          <Button variant="danger" onClick={handleDelete}>Confirm</Button>
+        </ModalFooter>
+
+      </Modal>
+    </Container>
+
     );
   };
 
--- src/postsRedux.js
+++ src/postsRedux.js
@@ -1,14 +1,20 @@
 //selectors
 export const getAllPosts= (state) => state.posts;
 
+
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 
 export const LOAD_POSTS= createActionName('LOAD_POSTS');
+export const REMOVE_POST=createActionName('REMOVE_POST');
+
+export const removePost=id=> ({type:REMOVE_POST, payload:id});
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter(post=>post.id !== action.payload);
     default:
       return statePart;
   };
